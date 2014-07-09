f3a7061e1c4790e53f65db4d449cc976e85501f3
Jeremy Boggs <jeremy@clioweb.org>
2014-07-09 14:06:36 +0200

    Obfuscates footer email addresses.

--- footer.php
+++ footer.php
@@ -11,8 +11,8 @@
             <span class="locality">Boston</span>, <span class="region">MA</span>, <span class="postal-code">02215</span>
         </div>
         <div class="tel">+1 617-358-6370</div>
-        <a class="email" href="mailto:secretary@ach.org">secretary@ach.org</a> ·
-        <a class="email" href="mailto:webmaster@ach.org">webmaster@ach.org</a>
+        <a class="email" href="mailto:<?php echo antispambot('secretary@ach.org'); ?>"><?php echo antispambot('secretary@ach.org'); ?></a> ·
+        <a class="email" href="mailto:<?php echo antispambot('webmaster@ach.org'); ?>"><?php echo antispambot('webmaster@ach.org'); ?></a>
     </div>
 
     <p class="license"><span class="entypo-cc"></span> Content licensed under <a href="http://creativecommons.org/licenses/by/3.0/">CC BY-SA 3.0</a></p>
